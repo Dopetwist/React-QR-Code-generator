aff39dcc50eef3c2c2bd61ace779bbe6cb352a5d
Jonathan <jonathanafugwobi1@gmail.com>
2025-10-29 14:46:10 +0100

    Add form element around input fields

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -21,25 +21,27 @@ function QrCode() {
         <div>
             <h1> QR Code Generator </h1>
             <div className="container">
-                <input type="text"
-                onChange={handleChange}
-                value={inputValue} 
-                placeholder="Enter Link or Phone Number" 
-                />
+                <form action="#">
+                    <input type="text"
+                    onChange={handleChange}
+                    value={inputValue} 
+                    placeholder="Enter Link or Phone Number" 
+                    />
 
-                <input type="text" value="" placeholder="Title of the QR Code" />
+                    <input type="text" value="" placeholder="Title of the QR Code" />
 
-                <input type="text" value="" placeholder="Enter Background Color" />
+                    <input type="text" value="" placeholder="Enter Background Color (optional)" />
 
-                <input type="text" value="" placeholder="Enter Foreground Color" />
+                    <input type="text" value="" placeholder="Enter Foreground Color (optional)" />
 
-                <input type="file" />
+                    <input type="file" />
 
-                <button className="btn"
-                onClick={handleClick}
-                > 
-                    Generate
-                </button>
+                    <button className="btn"
+                    onClick={handleClick}
+                    > 
+                        Generate
+                    </button>
+                </form>
 
                 <div className="img-con">
                     {code}
