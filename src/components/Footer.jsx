a03444b2303196c53fc81c4fe3e70e1de74e03a9
Jonathan <jonathanafugwobi1@gmail.com>
2025-11-26 00:53:59 +0100

    Create and add Footer component to app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+    return (
+        <div>
+            <p className="footer">  
+                &copy; {new Date().getFullYear()}, Made with ❤️ by Jonathan Afugwobi
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
